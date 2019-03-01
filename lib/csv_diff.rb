d975ce2863eae8aaae07b0719c9c1e288694071f
Anton Davydov <antondavydov.o@gmail.com>
2019-03-01 20:16:07 +0300

    Use module

--- lib/csv_diff.rb
+++ lib/csv_diff.rb
@@ -3,47 +3,44 @@ require 'csv'
 require "csv_diff/version"
 
 module CsvDiff
-  # Your code goes here...
-end
-
-
-class DiffDetector
-  def call(old_file, new_file)
-    group_changes(changed_lines(old_file, new_file))
-  end
+  class Detector
+    def call(old_file, new_file)
+      group_changes(changed_lines(old_file, new_file))
+    end
 
   private
 
-  def changed_lines(old_file, new_file)
-    Diffy::Diff.new(old_file, new_file, source: 'files', include_diff_info: false, context: false)
-      .map { |line| line[/^[+-]/] ? line : nil }
-      .compact.flatten
-  end
-
-  def group_changes(changed_lines)
-    changed_lines
-      .group_by { |line| line[/\d+/] }
-      .map { |_id, data| parse_changes(data) }
-  end
-
-  def parse_changes(data)
-    hash = {}
-    data.each do |line|
-      case line[0]
-      when '+' then hash[:new] = parse_change(line)
-      when '-' then hash[:old] = parse_change(line)
-      end
+    def changed_lines(old_file, new_file)
+      Diffy::Diff.new(old_file, new_file, source: 'files', include_diff_info: false, context: false)
+        .map { |line| line[/^[+-]/] ? line : nil }
+        .compact.flatten
     end
-    hash
-  end
 
-  def parse_change(line)
-    return unless line
-    CSV.parse(line[1..-1]).first
+    def group_changes(changed_lines)
+      changed_lines
+        .group_by { |line| line[/\d+/] }
+        .map { |_id, data| parse_changes(data) }
+    end
+
+    def parse_changes(data)
+      hash = {}
+      data.each do |line|
+        case line[0]
+        when '+' then hash[:new] = parse_change(line)
+        when '-' then hash[:old] = parse_change(line)
+        end
+      end
+      hash
+    end
+
+    def parse_change(line)
+      return unless line
+      CSV.parse(line[1..-1]).first
+    end
   end
 end
 
-diff = DiffDetector.new.call('./prices_old.csv', './prices.csv')
+diff = CsvDiff::Detector.new.call('./prices_old.csv', './prices.csv')
 
 p diff
 
